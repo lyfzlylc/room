fd1cdf8c347481fca5887a50a780cd43569ed735
望月 <2661937593@qq.com>
2023-05-04 08:05:55 +0000

    删除文件 hosue

--- hosue/src/main/java/com/wangyue/service/MusicService.java
+++
@@ -1,9 +0,0 @@
-package com.wangyue.service;
-
-import com.wangyue.domain.Music;
-
-import java.util.List;
-
-public interface MusicService {
-    List<Music> findAll();
-}
--- hosue/src/main/java/com/wangyue/service/UserlistService.java
+++
@@ -1,8 +0,0 @@
-package com.wangyue.service;
-
-import com.wangyue.domain.Userlist;
-import org.apache.ibatis.annotations.Param;
-
-public interface UserlistService {
-    Userlist findByUserid(Integer userid);
-}
